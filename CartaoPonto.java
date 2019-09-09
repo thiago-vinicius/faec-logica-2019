a1bc86d45fae50f6f5e23b735a51709c7d38de35
thiago-vinicius <55115425+thiago-vinicius@users.noreply.github.com>
2019-09-09 20:53:33 -0300

    Add files via upload
    Primeiro Comite de Teste

---
+++ CartaoPonto.java
@@ -0,0 +1,19 @@
+public class CartaoPonto {
+	
+	int em;
+	int sm;
+	int et;
+	int st;
+	int TotalMes;
+	
+	public static void main(String[] args) {
+	CartaoPonto[] dia = new CartaoPonto[30];
+	
+	void inicializaMes (CartaoPonto[] dia) {
+	CartaoPonto ArquivoMensal = new CartaoPonto();
+	ArquivoMensal.inicializaMes(dia);
+	}
+		public static void main()
+	}
+}
+}
